5d091ab8d437a692437accd392f9e3010176b0e6
Nestor Lozada <martinlozada1@gmail.com>
2024-11-23 22:23:31 -0500

    trato de hacer que funque

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressB
 import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
 import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
@@ -185,6 +185,8 @@ const router = createRouter({
       component: ElTypography,
     },
   ],
+  
 });
+
 
 export default router;
