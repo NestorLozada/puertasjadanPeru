2d8e0f2c82dc0a8f6c4c4b2cc0367d43d29f7c2a
Nestor Lozada <martinlozada1@gmail.com>
2024-11-23 22:17:27 -0500

    correcion de vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Configura la aplicación para desplegar en la raíz
   plugins: [vue()],
   resolve: {
     alias: {
